d4b021d034da14a067e3d42779c1489ec328a86c
MattWong-ca <matthew.yt.wong@gmail.com>
2023-12-27 15:35:56 -0600

    Add sidebar

--- apps/web/src/components/Home/Home.tsx
+++ apps/web/src/components/Home/Home.tsx
@@ -1,9 +1,11 @@
 import styles from './Home.module.css'
 import { Link } from 'react-router-dom';
+import { Sidebar } from '../Sidebar/Sidebar';
 
 export const Home = () => {
   return (
     <div>
+        <Sidebar/>
         <p>This will be the dashboard!</p>
     </div>
   )
---
+++ apps/web/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,10 @@
+import styles from './Home.module.css'
+import { Link } from 'react-router-dom';
+
+export const Sidebar = () => {
+  return (
+    <div>
+        <p>This will be the sidebar!</p>
+    </div>
+  )
+}
