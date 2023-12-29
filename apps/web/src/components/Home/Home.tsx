2a3560ac038448139fd6c3434443d73d28b22f04
MattWong-ca <matthew.yt.wong@gmail.com>
2023-12-28 20:05:25 -0500

    Update sidebar

--- apps/web/src/components/Home/Home.tsx
+++ apps/web/src/components/Home/Home.tsx
@@ -1,12 +1,22 @@
 import styles from './Home.module.css'
 import { Link } from 'react-router-dom';
 import { Sidebar } from '../Sidebar/Sidebar';
+import React, { useState } from 'react';
 
 export const Home = () => {
-  return (
-    <div>
-        <Sidebar/>
-        <p>This will be the dashboard!</p>
-    </div>
-  )
+    const [activeTab, setActiveTab] = useState('Dashboard');
+
+    return (
+
+        <div className="dashboard-container">
+            <Sidebar setActiveTab={setActiveTab} activeTab={activeTab} />
+            <div className="main-content">
+                {/* Render content based on the activeTab */}
+                {activeTab === 'Dashboard' && <h2>Dashboard Content</h2>}
+                {activeTab === 'Accounts' && <h2>Accounts Content</h2>}
+                {activeTab === 'Transactions' && <h2>Transactions Content</h2>}
+                {activeTab === 'Settings' && <h2>Settings Content</h2>}
+            </div>
+        </div>
+    )
 }
--- apps/web/src/components/Sidebar/Sidebar.tsx
+++ apps/web/src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,19 @@
 import styles from './Home.module.css'
 import { Link } from 'react-router-dom';
 
-export const Sidebar = () => {
-  return (
-    <div>
-        <p>This will be the sidebar!</p>
-    </div>
-  )
+export const Sidebar = ({ setActiveTab, activeTab }) => {
+    const tabs = ['Dashboard', 'Accounts', 'Transactions', 'Settings'];
+    return (
+        <div className="sidebar">
+            {tabs.map(tab => (
+                <div
+                    key={tab}
+                    className={`sidebar-item ${activeTab === tab ? 'active' : ''}`}
+                    onClick={() => setActiveTab(tab)}
+                >
+                    {tab}
+                </div>
+            ))}
+        </div>
+    )
 }
