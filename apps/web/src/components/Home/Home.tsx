bedc928ac753945f165cea0a972e630708ffac88
MattWong-ca <matthew.yt.wong@gmail.com>
2023-12-30 00:00:16 -0500

    Fix main dashboard CSS styling

--- apps/web/src/components/Home/Home.tsx
+++ apps/web/src/components/Home/Home.tsx
@@ -1,5 +1,6 @@
 import styles from './Home.module.css'
 import { Sidebar } from '../Sidebar/Sidebar';
+import { MainDashboard } from '../MainDashboard/MainDashboard';
 import { useState } from 'react';
 
 export const Home = () => {
@@ -10,7 +11,7 @@ export const Home = () => {
             <Sidebar setActiveTab={setActiveTab} activeTab={activeTab} />
             <div className={styles.mainContent}>
                 {/* Render content based on the activeTab */}
-                {activeTab === 'Dashboard' && <h2>Dashboard Content</h2>}
+                {activeTab === 'Dashboard' && <MainDashboard></MainDashboard>}
                 {activeTab === 'Accounts' && <h2>Accounts Content</h2>}
                 {activeTab === 'Transactions' && <h2>Transactions Content</h2>}
                 {activeTab === 'Settings' && <h2>Settings Content</h2>}
---
+++ apps/web/src/components/MainDashboard/MainDashboard.tsx
@@ -0,0 +1,17 @@
+import styles from './MainDashboard.module.css'
+import { Link } from 'react-router-dom';
+
+export const MainDashboard = () => {
+    return (
+        <div style={{ display: 'inline-flex', position: 'relative', width: '100%', height: '100vh'  }}>
+            <div style={{ backgroundColor: 'blue', width: '80%', overflow: 'auto' }}>
+                <div>
+                    
+                </div>
+            </div>
+            <div style={{ height: '100vh', flex: 1 }}>
+                T
+            </div>
+        </div>
+    )
+}
